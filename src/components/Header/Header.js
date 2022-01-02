433a090b9098fe2ff338c7dc02e62ee40e9bec54
Carlos Fernandez <fdez.carlos303@gmail.com>
2022-01-01 19:20:53 -0500

    Header work

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,41 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { Icon } from "@iconify/react";
 import Typical from "react-typical";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./Header.css";
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: "Franklin Gothic Medium",
+    },
+  },
+  palette: {
+    primary: {
+      main: "#000",
+    },
+  },
+});
+
 export default function Header() {
   return (
     <Container>
       <Row>
-        <Col>
-          <Icon icon="la:laptop-code" />
+        <Col className="header-container">
+          <Icon className="header-laptop-icon" icon="la:laptop-code" />
+          <Typical className="header-name" steps={["Carlos Fernandez"]} />
+          <Typical className="header-job" steps={["Web Developer"]} />
+          <div className="header-links-container">
+            <Icon className="github-icon" icon="akar-icons:github-fill" />
+            <Icon className="linkedin-icon" icon="akar-icons:linkedin-fill" />
+            <ThemeProvider theme={theme}>
+              <Button className="resume-btn" variant="contained">
+                Resume
+              </Button>
+            </ThemeProvider>
+          </div>
         </Col>
-      </Row>
-      <Row>
-        <Typical className="name" steps={["Carlos Fernandez"]} />
-      </Row>
-      <Row>
-        <Typical steps={["Web Developer"]} />
       </Row>
     </Container>
   );
