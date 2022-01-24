c47d10515c7d12fe519c335ca01c1eed8eabf204
Carlos Fernandez <fdez.carlos303@gmail.com>
2022-01-23 21:11:02 -0500

    .

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,11 +12,21 @@ export default function About() {
         <Col className="about-me-photo">
           <div className="img-container">
             <img src="about-me-photo.jpg" alt="Carlos Fernandez" />
-            <div className="job-logos">
-              <Icon icon="logos:javascript" />
-              <Icon icon="logos:react" />
-              <Icon icon="logos:python" />
+            <div className="job-logos-container">
+              <Icon className="job-logos" icon="logos:javascript" />
+              <Icon className="job-logos" icon="logos:react" />
+              <Icon className="job-logos" icon="logos:python" />
             </div>
+          </div>
+        </Col>
+
+        <Col>
+          <div className="about-me-description">
+            <h1>Hello!</h1>
+            <p>
+              My name is Carlos Fernandez and I will make this description a lot
+              better trust
+            </p>
           </div>
         </Col>
       </Row>
